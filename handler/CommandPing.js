86db3b08143c5b2f392564d1ed31bcd934860541
Yorushika <yorushika@outlook.com>
2020-02-24 22:58:39 +0800

    switch command .ping result from ms to s

--- handler/CommandPing.js
+++ handler/CommandPing.js
@@ -2,9 +2,10 @@ const { CommandHandler } = require('../lib/type/handler');
 
 const ping = new CommandHandler('ping', [], '通信延迟', async (session) => {
   const timeStamp = Date.now();
-  let latency = timeStamp - session.msgJson.time * 1000;
-  latency = parseInt(latency, 10);
-  session.send(`${latency}ms`);
+  let latency = timeStamp / 1000 - session.msgJson.time;
+  latency = latency.toString();
+  latency = latency.slice(0, latency.indexOf('.') + 3);
+  session.send(`${latency}s`);
 });
 
 module.exports = [ping];
